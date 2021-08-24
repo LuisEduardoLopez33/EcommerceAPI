6d492dc3d9a75752118639e295a22162d28ac1db
Uziel <201274@ids.upchiapas.edu.mx>
2021-08-24 14:51:48 -0500

    Pequeño avance

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/', function (req, res){
+    res.send('Estas entrando en la Parte de Cars')
+})
 router.post('/add');
 router.get('/consult');
 router.post('/delete');
