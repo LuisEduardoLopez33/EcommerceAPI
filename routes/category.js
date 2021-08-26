3c9074728cc69c08c64ef17d25b779331d207548
Jahr2001 <193213@ids.upchiapas.edu.mx>
2021-08-25 23:19:10 -0500

    Advancement in Category

--- routes/category.js
+++ routes/category.js
@@ -4,15 +4,20 @@ const category = require('../controllers/category');
 
 
 router.get('/',(req, res) => {
+
     res.send('CATEGORIAS!!!!');
+
 })
 
 router.get('/getCategories', category.getAllCategories);
+
 router.get('getCategory/:idCategory', category.consultCategories);
 
 //
 router.post('/addCategory', category.insertCategory);
+
 router.put('/updateCategory', category.updateCategory);
+
 router.delete('/deleteCategory', category.deleteCategory);
 
 
